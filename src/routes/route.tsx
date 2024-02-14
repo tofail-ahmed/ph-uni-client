a5d47bf79b68e1b86be033a25fd52623326856b1
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-02-14 19:52:05 +0600

    admin routes added

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import { AlibabaOutlined } from "@ant-design/icons";
 import About from "../pages/About";
 import Contact from "../pages/Contact";
 import Login from "../pages/Login";
@@ -8,7 +7,9 @@ import Register from "../pages/Register";
 import Phone from "../pages/Phone";
 import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateStudent from "../pages/admin/CreateStudent";
-import AdminLayout from "../components/layout/AdminLayout";
+import CreateAdmin from "../pages/admin/CreateAdmin";
+import CreateFaculty from "../pages/admin/CreateFaculty";
+import { adminPaths } from "./admin.routes";
 
 const router = createBrowserRouter([
   {
@@ -34,21 +35,17 @@ const router = createBrowserRouter([
   {
     path: "/admin",
     element: <App />,
-    children: [
-      {
-       index:true,
-        element: <AdminDashboard />,
-      },
-      {
-       path:"dashboard",
-        element: <AdminDashboard />,
-      },
-      {
-        path: "createstudent",
-        element: <CreateStudent />,
-       
-      },
-    ],
+    children: adminPaths,
+  },
+  {
+    path: "/faculty",
+    element: <App />,
+    children: adminPaths,
+  },
+  {
+    path: "/student",
+    element: <App />,
+    children: adminPaths,
   },
 
   {
