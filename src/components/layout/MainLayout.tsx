dd0d8b25e9ff875ed07b37c03a0bdf3e499fc1ff
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-02-14 12:06:14 +0600

    antd sidebar added to main layout

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Layout, Menu } from 'antd';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+
+
+const { Header, Content, Footer, Sider } = Layout;
+
+
+const items = [UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
+  (icon, index) => ({
+    key: String(index + 1),
+    icon: React.createElement(icon),
+    label: `nav ${index + 1}`,
+  }),
+);
+const MainLayout = () => {
+
+  return (
+    <Layout style={{height:"100vh"}}>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div className="demo-logo-vertical" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0}} />
+        <Content style={{ margin: '24px 16px 0' }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              
+            }}
+          >
+            The main content should place over there
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  )
+}
+
+export default MainLayout
