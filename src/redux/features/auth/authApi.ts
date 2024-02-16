ff02d33f86f8018522bd88e22f30f5524ad042f3
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-02-16 23:24:27 +0600

    auth api and baseApi initiated

---
+++ src/redux/features/auth/authApi.ts
@@ -0,0 +1,13 @@
+import { baseApi } from "../../api/baseApi";
+
+const authApi=baseApi.injectEndpoints({
+      endpoints: (builder) => ({
+            login: builder.mutation({
+              query: (userInfo) => ({
+                url: "/auth/login",
+                method: "POST",
+                body: userInfo,
+              }),
+            }),
+          }),
+})
