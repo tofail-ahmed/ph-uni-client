c58649e909feb1c676cbbc00d64e220de5456062
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-02-17 12:56:22 +0600

    jwtDecode added for decoding accessToken to retreive user details

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,28 +2,37 @@ import { Button } from 'antd';
 import React from 'react'
 import { useForm } from 'react-hook-form';
 import { useLoginMutation } from '../redux/features/auth/authApi';
+import { useAppDispatch } from '../redux/hooks';
+import { setUser } from '../redux/features/auth/authSlice';
+import { verifyToken } from '../utils/verifyToken';
 
 const Login = () => {
-
-  const {register,handleSubmit}=useForm({
-    defaultValues:{
-      id:"A-0002",
-      password:"admin123"
-    }
+  const dispatch=useAppDispatch();
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      id: "A-0002",
+      password: "admin123",
+    },
   });
-  const [login,{data,error}]=useLoginMutation();
-  console.log(data)
-  console.log(error)
-const onSubmit=(data)=>{
-  console.log(data)
-  const userInfo={
-    id:data.id,
-    password:data.password
-  }
-  login(userInfo)
-}
+  const [login, { data, error }] = useLoginMutation();
+  // console.log(data)
+  // console.log(error)
+  const onSubmit = async (data) => {
+    console.log(data);
+    const userInfo = {
+      id: data.id,
+      password: data.password,
+    };
+    const res = await login(userInfo).unwrap();
+    const user=verifyToken(res.data.accessToken)
+dispatch(setUser({
+  user:user,
+  token:res.data.accessToken
+}))
+    // console.log(res.data.accessToken);
+  };
   return (
-    <div style={{display:"flex", justifyContent:"center"}}>
+    <div style={{ display: "flex", justifyContent: "center" }}>
       <div>
         <form onSubmit={handleSubmit(onSubmit)} action="">
           <div>
@@ -32,9 +41,9 @@ const onSubmit=(data)=>{
           </div>
           <div>
             <label htmlFor="password">Password:</label>
-            <input type="text" id="password" {...register("password")}/>
+            <input type="text" id="password" {...register("password")} />
           </div>
-          <Button htmlType='submit'>Submit</Button>
+          <Button htmlType="submit">Submit</Button>
         </form>
       </div>
     </div>
