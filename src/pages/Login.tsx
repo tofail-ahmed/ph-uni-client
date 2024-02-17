200bda20af32409e0cf674db445c9ba0e4b62337
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-02-17 10:59:54 +0600

    react-hook-from initiated

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,44 @@
+import { Button } from 'antd';
 import React from 'react'
+import { useForm } from 'react-hook-form';
+import { useLoginMutation } from '../redux/features/auth/authApi';
 
 const Login = () => {
+
+  const {register,handleSubmit}=useForm({
+    defaultValues:{
+      id:"A-0001",
+      password:"admin123"
+    }
+  });
+  const [login,{data,error}]=useLoginMutation();
+  console.log(data)
+  console.log(error)
+const onSubmit=(data)=>{
+  console.log(data)
+  const userInfo={
+    id:data.id,
+    password:data.password
+  }
+  login(userInfo)
+}
   return (
-    <div>Login</div>
-  )
+    <div style={{display:"flex", justifyContent:"center"}}>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)} action="">
+          <div>
+            <label htmlFor="id">ID:</label>
+            <input type="text" id="id" {...register("id")} />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input type="text" id="password" {...register("password")}/>
+          </div>
+          <Button htmlType='submit'>Submit</Button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login
--- src/redux/features/auth/authApi.ts
+++ src/redux/features/auth/authApi.ts
@@ -11,3 +11,5 @@ const authApi=baseApi.injectEndpoints({
             }),
           }),
 })
+
+export const {useLoginMutation}=authApi;
