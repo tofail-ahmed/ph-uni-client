321220f2342f14a064a4df89c7917730c2cb5cbf
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-03-07 21:52:29 +0600

    createAcacdemicSemester smaple fporm added

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import PHForm from '../../../components/form/PHForm'
+import PHInput from '../../../components/form/PHInput'
+import { FieldValues, SubmitHandler } from 'react-hook-form'
+import { Button } from 'antd'
 
 const CreateAcademicSemester = () => {
+      const onSubmit:SubmitHandler<FieldValues>=(data)=>{
+            console.log(data)
+      }
   return (
-    <div>CreateAcademicSemester</div>
+    <PHForm onSubmit={onSubmit}>
+      <PHInput type='text' name="name"></PHInput>
+      <Button htmlType='submit' >Submit</Button>
+    </PHForm>
   )
 }
 
