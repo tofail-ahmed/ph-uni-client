6e031901bfdbbd0682ecd52db319bd4dd1899241
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-03-18 22:14:49 +0600

    userManagement type added

--- src/redux/features/academicSemester/userManagementApi.ts
+++ src/redux/features/academicSemester/userManagementApi.ts
@@ -1,29 +1,30 @@
+import { TQueryParam, TResponseRedux } from "../../../types/global";
 import { baseApi } from "../../api/baseApi";
 
 const userManagementApi=baseApi.injectEndpoints({
       endpoints: (builder) => ({
-            // getAllSemesters: builder.query({
-            //   query: (args) => {
-            //     const params=new URLSearchParams();
-            //    if(args){
-            //     args.forEach((item:TQueryParam)=>{
-            //       params.append(item.name,item.value as string)
-            //     })
-            //    }
-            //    return {
-            //     url: "/academic-semesters",
-            //     method: "GET",
-            //     params:params,
+            getAllStudents: builder.query({
+              query: (args) => {
+                const params=new URLSearchParams();
+               if(args){
+                args.forEach((item:TQueryParam)=>{
+                  params.append(item.name,item.value as string)
+                })
+               }
+               return {
+                url: "/academic-semesters",
+                method: "GET",
+                params:params,
             
-            //    }
-            //   },
-            //   transformResponse:(response:TResponseRedux<TAcademicSemester[]>)=>{
-            //     return {
-            //       data:response.data,
-            //       meta:response.meta,
-            //     }
-            //   }
-            // }),
+               }
+              },
+              transformResponse:(response:TResponseRedux<TAcademicSemester[]>)=>{
+                return {
+                  data:response.data,
+                  meta:response.meta,
+                }
+              }
+            }),
             addStudent:builder.mutation({
               query:(data)=>({
                 url:"/users/create-student",
@@ -33,4 +34,4 @@ const userManagementApi=baseApi.injectEndpoints({
             })
           })
 })
-export const {useAddStudentMutation}=userManagementApi;
+export const {useAddStudentMutation,useGetAllStudentsQuery}=userManagementApi;
