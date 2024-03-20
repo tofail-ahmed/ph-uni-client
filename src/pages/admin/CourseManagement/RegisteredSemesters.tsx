d3868027dee21ee0f6cce85ee1fce07d6c95441e
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-03-20 12:19:32 +0600

    Refactor Table Date And Handle Status Change Using Antd Dropdown

--- src/pages/admin/CourseManagement/RegisteredSemesters.tsx
+++ src/pages/admin/CourseManagement/RegisteredSemesters.tsx
@@ -1,29 +1,44 @@
-import { Button, Table, TableColumnsType, TableProps } from "antd";
-import { useGetAllSemestersQuery } from "../../../redux/features/academicSemester/academicSemesterApi";
-import { TAcademicSemester } from "../../../types/academicManagement.type";
-import { useState } from "react";
-import { TQueryParam } from "../../../types/global";
+import { Button, Dropdown, Table, TableColumnsType, TableProps, Tag } from "antd";
 import { useGetAllRegisteredSemestersQuery } from "../../../redux/features/academicSemester/CourseManagementApi";
-export type TTabledata = Pick<
-  TAcademicSemester,
-  "name" | "year" | "endMonth" | "startMonth"
->;
-
+import { TSemester } from "../../../types/courseManagement.types";
+import moment from "moment";
+export type TTabledata = Pick<TSemester, "startDate" | "endDate" | "status">;
+const items=[
+  {
+    label:"Ongoing",
+    key:"ONGOING"
+  },
+  {
+    label:"Upcoming",
+    key:"UPCOMING"
+  },
+  {
+    label:"Ended",
+    key:"ENDED"
+  },
+];
 const RegisteredSemesters = () => {
-const {data:semesterData,isLoading,isFetching}=useGetAllRegisteredSemestersQuery(undefined)
- 
-  // console.log(semesterData);
-  
-  const tableData = semesterData?.data?.map(
-    ({ _id, academicSemester, startDate, endDate,status }) => ({
-      key: _id,
-      name:`${academicSemester.name} ${academicSemester.year}`,
-      startDate, endDate,status
-     
+  const { data: semesterData, isFetching } =
+    useGetAllRegisteredSemestersQuery(undefined);
 
-     
+  // console.log(semesterData);
+
+  const tableData = semesterData?.data?.map(
+    ({ _id, academicSemester, startDate, endDate, status }) => ({
+      key: _id,
+      name: `${academicSemester.name} ${academicSemester.year}`,
+      startDate: moment(new Date(startDate)).format("MMMM") ,
+      endDate: moment(new Date(endDate)).format("MMMM"),
+      status,
     })
   );
+  const handleStatusDropdown=(data)=>{
+    console.log(data)
+  };
+  const menuProps={
+    items,
+    onClick:handleStatusDropdown
+  }
   const columns: TableColumnsType<TTabledata> = [
     {
       title: "Name",
@@ -34,6 +49,17 @@ const {data:semesterData,isLoading,isFetching}=useGetAllRegisteredSemestersQuery
       title: "Status",
       key: "status",
       dataIndex: "status",
+      render:(item)=>{
+        let color;
+        if(item==="UPCOMING"){
+          color="blue"
+        }else if(item==="ONGOING"){
+          color="green"
+        }else{
+          color="red"
+        }
+        return <Tag style={{ fontWeight: 'bold' }} color={color}>{item}</Tag>
+      }
     },
     {
       title: "Start Date",
@@ -50,9 +76,9 @@ const {data:semesterData,isLoading,isFetching}=useGetAllRegisteredSemestersQuery
       key: "X",
       render: () => {
         return (
-          <div>
+          <Dropdown menu={menuProps}>
             <Button>Update</Button>
-          </div>
+          </Dropdown>
         );
       },
     },
