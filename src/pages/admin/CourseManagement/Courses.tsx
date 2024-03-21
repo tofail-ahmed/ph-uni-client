1856df75bb38b62b9d1d66e4ff7c28a6b2a12a86
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-03-21 12:17:33 +0600

    Handle Modal With Ant Design

--- src/pages/admin/CourseManagement/Courses.tsx
+++ src/pages/admin/CourseManagement/Courses.tsx
@@ -1,55 +1,51 @@
-import { Button, Table, TableColumnsType, TableProps } from "antd";
+import { Button, Modal, Table, TableColumnsType, TableProps } from "antd";
 import { useGetAllSemestersQuery } from "../../../redux/features/academicSemester/academicSemesterApi";
 import { TAcademicSemester } from "../../../types/academicManagement.type";
 import { useState } from "react";
 import { TQueryParam } from "../../../types/global";
 import { useGetAllCoursesQuery } from "../../../redux/features/academicSemester/CourseManagementApi";
-export type TTabledata=Pick<TAcademicSemester,"name"|"year"|"endMonth"|"startMonth">
+import PHSelect from "../../../components/form/PHSelect";
+import PHForm from "../../../components/form/PHForm";
+import { useGetAllFacultiesQuery } from "../../../redux/features/academicSemester/userManagementApi";
+// export type TTabledata=Pick<TAcademicSemester,"name"|"year"|"endMonth"|"startMonth">
 
 const Courses = () => {
   // const [params,setParams]=useState<TQueryParam[]|undefined>(undefined);
   // const { data: semesterData } = useGetAllSemestersQuery([
   //   {name:"year",value:"2025"}
   // ]);
-  const { data: courses,isFetching } = useGetAllCoursesQuery(undefined);
+  const { data: courses, isFetching } = useGetAllCoursesQuery(undefined);
   // console.log(semesterData);
   // const passYear = (year:string) => {
   //   return parseInt(year)+4;
   // };
-  const tableData = courses?.data?.map(
-    ({ _id, title, prefix, code,  }) => ({
-      key:_id,
-      title, prefix, code:`${prefix} ${code}`
-
-      
-    })
-  );
+  const tableData = courses?.data?.map(({ _id, title, prefix, code }) => ({
+    key: _id,
+    title,
+    prefix,
+    code: `${prefix} ${code}`,
+  }));
   const columns = [
     {
       title: "Title",
-      key:"title",
+      key: "title",
       dataIndex: "title",
-     
     },
     {
       title: "Code",
-      key:"code",
+      key: "code",
       dataIndex: "code",
-     
     },
-    
+
     {
-      title:"Action",
-      key:"X",
-      render:(item)=>{
-        return <div>
-          <Button>Update</Button>
-        </div>
-      }
-    }
+      title: "Action",
+      key: "X",
+      render: (item) => {
+        return <AddFacultyModal data={item} />;
+      },
+    },
   ];
 
- 
   // const onChange: TableProps<TTabledata>["onChange"] = (
   //   _pagination,
   //   filters,
@@ -66,7 +62,42 @@ const Courses = () => {
   //     console.log(queryParams)
   //   }
   // };
-  return <Table loading={isFetching} columns={columns} dataSource={tableData}  />;
+  return (
+    <Table loading={isFetching} columns={columns} dataSource={tableData} />
+  );
 };
 
-export default Courses
+const AddFacultyModal = ({ data }) => {
+  console.log(data.key);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { data: facultiesData } = useGetAllFacultiesQuery(undefined);
+  
+  const facultiesOption=facultiesData?.data?.map((item)=>({
+    value:item._id,
+    label:item.fullName,
+  }))
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <Button onClick={showModal}>Add Faculty</Button>
+      <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk}>
+        <PHForm onSubmit={handleSubmit}>
+          <PHSelect mode="multiple" options={facultiesOption} name="faculties" label="Faculty" />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Modal>
+    </>
+  );
+};
+
+export default Courses;
--- src/redux/features/academicSemester/userManagementApi.ts
+++ src/redux/features/academicSemester/userManagementApi.ts
@@ -26,6 +26,28 @@ const userManagementApi=baseApi.injectEndpoints({
                 }
               }
             }),
+            getAllFaculties: builder.query({
+              query: (args) => {
+                const params=new URLSearchParams();
+               if(args){
+                args.forEach((item:TQueryParam)=>{
+                  params.append(item.name,item.value as string)
+                })
+               }
+               return {
+                url: "/faculties",
+                method: "GET",
+                params:params,
+            
+               }
+              },
+              transformResponse:(response:TResponseRedux<TStudent[]>)=>{
+                return {
+                  data:response.data,
+                  meta:response.meta,
+                }
+              }
+            }),
             addStudent:builder.mutation({
               query:(data)=>({
                 url:"/users/create-student",
@@ -35,4 +57,4 @@ const userManagementApi=baseApi.injectEndpoints({
             })
           })
 })
-export const {useAddStudentMutation,useGetAllStudentsQuery}=userManagementApi;
+export const {useAddStudentMutation,useGetAllStudentsQuery,useGetAllFacultiesQuery}=userManagementApi;
